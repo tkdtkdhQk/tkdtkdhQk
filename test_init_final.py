73406f9d8580e9216b540c8182325f7cb2c40029
tkdtkdhQk <63703726+tkdtkdhQk@users.noreply.github.com>
2020-06-29 22:57:58 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -279,8 +279,8 @@ def init():
 			endTime = endTime + datetime.timedelta(days=int(basicSetting[13]))
 
 	### 채널 고정###
-	#basicSetting[6] = int('597781866681991198') #보이스채널ID
-	#basicSetting[7] = int('597782016607649829') #택스트채널ID
+	basicSetting[6] = int('701543886455177236') #보이스채널ID
+	basicSetting[7] = int('701543792758620251') #택스트채널ID
 	
 	bossNum = int(len(boss_inputData)/5)
 
